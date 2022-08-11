2d7ce9ea4e0d2e43883728e419f17e942d1341a5
Nick <Nick_II2000@mail.ru>
2022-08-11 21:36:31 +0600

    15-th commit

--- script.js
+++ script.js
@@ -11,3 +11,106 @@ tns({
     mode: "carousel",
     speed: 300,
 });
+//==============================================
+
+
+const arr = [
+    {
+        name: 'Alex',
+        salary: 500
+    },
+    {
+        name: 'Ann',
+        salary: 1500
+    },
+    {
+        name: 'John',
+        salary: 2500
+    },
+];
+
+const result = arr.map(item => Object.entries(item)[1][1]).reduce((sum, curr) => sum + curr);
+console.log(result);
+
+
+const msg = 'My number +12345678, name: Oleg';
+
+function transformMsg(str) {
+
+    let a = str.replace(/\+\d{8}/, "*****");
+    let b = a.replace(/\w{4}:\s\w{1,}/, "hidden");
+
+    return b;
+}
+
+console.log(transformMsg(msg));
+// Результатом вызова этой функции сейчас будет:
+//My number *****, hidden
+
+
+
+let y = 1;
+let x = y = 2;
+console.log(x);
+
+
+
+let c = 4;
+function addX(x) {
+    return function (n) {
+        return n + x
+    }
+}
+
+const addThree = addX(3);
+
+let d = addThree(c);
+let res = addThree(c);
+
+console.log(res)
+
+
+
+const promisify = (item, delay) =>
+    new Promise(resolve => setTimeout(() => resolve(item), delay));
+
+const a = () => promisify('a', 100);
+const b = () => promisify('b', 5000);
+const c = () => promisify('c', 3000);
+
+async function one() {
+    const promises = [a(), b(), c()];
+    const [outpu1, outpu2, outpu3] = await Promise.all(promises);
+    return `one is done: ${outpu1} ${outpu2} ${outpu3}`;
+}
+
+async function two() {
+    const promises = [a(), b(), c()];
+    const outpu1 = await Promise.race(promises);
+    return `two is done: ${outpu1}`;
+}
+
+async function three() {
+    const outpu1 = await a();
+    const outpu2 = await b();
+    const outpu3 = await c();
+    return `three is done: ${outpu1} ${outpu2} ${outpu3}`
+}
+
+one().then(console.log);
+two().then(console.log);
+three().then(console.log);
+
+
+'use strict';
+
+function getSum(a, b) {
+    function sum() {
+        console.log(this.a);
+        return a + b;
+    }
+
+    console.log(sum());
+}
+
+getSum(4, 5);
