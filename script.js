5e429b9be3fa809852d27f44a43c2a658c47e70e
Nick <Nick_II2000@mail.ru>
2022-08-13 23:25:24 +0600

    22-th commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ import { tns } from "./node_modules/tiny-slider/src/tiny-slider";
 tns({
     container: '.my-slider',
     items: 3,
-    slideBy: 'page',
+    // slideBy: 'page',
     autoplay: true,
     mode: "carousel",
     // speed: 300,
